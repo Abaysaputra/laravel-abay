b2fff3ba76c2eb9117325acca7db73fb0be5de04
unknown <adibayusaputra245@gmail.com>
2023-03-16 11:45:38 +0700

    continue part

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,5 +3,5 @@
     <h1>About</h1>
     <h3>{{ $name }}</h3>
     <h3>{{ $email }}</h3>
-    <img src="../img/{{ $image }}" alt="{{ $name }}" width="200" height="150px">
+    <img src="img/{{ $image }}" alt="{{ $name }}" width="200" height="280" class="img-tumbhnail rounded-circle">
 @endsection
